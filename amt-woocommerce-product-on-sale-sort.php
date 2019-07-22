ac4fb5b6e8ed5552df3a2b83b1b263efde3a648e
Amit Sharma <amitworx@gmail.com>
2019-07-22 11:05:12 +0530

    Bug Fixed - Include all products whether meta key _sale_price is set or not

--- amt-woocommerce-product-on-sale-sort.php
+++ amt-woocommerce-product-on-sale-sort.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: Amt woocommerce Product On Sale Sort
+Plugin Name: Amt Product On Sale Sort
 Description: This plugin adds option ' Sort by on sale ' to product sorting dropdown menu for woocommerce shop, and activates the default sorting for the products on an archive page for 'Sort by on sale'.
 Author:      Amit Sharma
 Author URI:  https://amitsharma.dev/
-Version:     1.0
+Version:     1.1
 License:     GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.txt
 
@@ -40,7 +40,9 @@ function amt_catalog_orderby( $sortby ) {
 add_filter('woocommerce_default_catalog_orderby', 'amt_default_catalog_orderby');
 
 function amt_default_catalog_orderby() {
+    // die('default sorting');
     return 'on_sale'; 
+   
 }
 
 
@@ -52,54 +54,44 @@ add_filter( 'woocommerce_get_catalog_ordering_args', 'amt_get_catalog_ordering_a
 function amt_get_catalog_ordering_args( $args ) {
 	$orderby_value = isset( $_GET['orderby'] ) ? wc_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
 	if ( 'on_sale' == $orderby_value ) {
-      // get current categoy ID
-       $current_category = get_queried_object();
-       $current_cat_id = $current_category->term_id;
 
-       // querying product for current category ID and where meta_key _sale_price is > 0  
-       $product_args = array(
-            'post_type'             => 'product',
-            'post_status'           => 'publish',
-            'ignore_sticky_posts'   => 1,
-            'posts_per_page'        => '-1',
-            'tax_query'             => array(
+
+        $current_category = get_queried_object();
+        
+        // var_dump($current_category);
+       
+        add_action( 'woocommerce_product_query', 'amt_on_sale_query' );
+
+        function amt_on_sale_query( $q ){
+            $meta_query = $q->get( 'meta_query' );
+            $meta_query = array( 
+                'relation' => 'OR',
                 array(
-                    'taxonomy'      => 'product_cat',
-                    'field'         => 'term_id',
-                    'terms'         => $current_cat_id,
-                    'operator'      => 'IN' 
-                )
-              
-            ),
-            'meta_query' => array(
+                    'key' => '_sale_price',
+                    'compare' => 'NOT EXISTS'
+                ),
                 array(
-                    'key'     => '_sale_price',
-                    'value'   => 0,
-                    'compare' => '>',
-                )
-            )
-        );
+                    'relation' => 'OR',
+                    array(
+                        'key' => '_sale_price',
+                        'value'=>array(''),
+                        'compare' => 'IN'
+                    ),
+                    array(
+                        'key' => '_sale_price',
+                        'value'=>0,
+                        'compare' => '>=',
+                        'type'   => 'numeric',
+                    )
+                ),
+            );
+           
+            $q->set('meta_query', $meta_query );
+            $q->set('orderby', array( 'meta_value' => 'DESC', 'date' => 'ASC' ));
+ 
+        }
 
-        // initialize the counter for number of products in the current category which are on sale
-        $i=0;
-        $products = new WP_Query($product_args);
-        while ($products->have_posts()) : 
-            $products->the_post();
-            $i++;
-        endwhile;
-
-        // modify the query if we have products on sale in a category
-        if($i>0){
-        $args['orderby'] = array(
-            'meta_value_num' => 'DESC', 'title' => 'ASC'
-           );
-           $args['meta_key'] = '_sale_price';
-        }else{
-	  $args['orderby'] = 'title';
-          $args['order'] = 'ASC';	
-	}
-	}
-	return $args;
+    }
 }
 
 
